a0cbe46aa8642c5861482b39fabdae503180ae9d
Selfmade20 <tebogo1686@gmail.com>
2019-10-03 17:44:37 +0200

    Changes

---
+++ Side-Projects/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './App.css';
+import MyInfo from './Components/MyInfo'
+
+function App() {
+  return (
+    <div>
+  <MyInfo/>
+    </div> );
+}
+
+export default App;
