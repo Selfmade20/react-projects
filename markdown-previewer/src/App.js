5898d85efde80daa6c438064cf6731a86b246344
Selfmade20 <tebogo1686@gmail.com>
2019-10-07 14:22:38 +0200

    Markdown previewer

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown'
 
 
-function App() {
+
+class App extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+          source: "markdown"
+      }  
+  }
+
+  handleChange(e) {
+      this.setState({ source: e.target.value });
+  }
+  
+  render(){
   return (
-    <div className="App">
-     <h1>Markdown Previewer</h1>
+    <div>
+      <h1>Markdown Previewer</h1>
+      <div className="container">
+        <h1> Markdown Previewer</h1>
+        <textarea rows="18" cols="48" onChange={this.handleChange.bind(this)} />
+        <ReactMarkdown source={this.state.source} />
+      </div>
     </div>
+
   );
+}
 }
 
 export default App;
