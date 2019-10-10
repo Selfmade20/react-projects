c40df39eba2b2cf7c2123ebb8a663ec263920391
Selfmade20 <tebogo1686@gmail.com>
2019-10-10 17:18:16 +0200

    Modified

--- thunk-example/src/redux/store/index.js
+++ thunk-example/src/redux/store/index.js
@@ -1,12 +1,34 @@
-import rootReducer from '.redux/rootReducer'
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from '../rootReducer';
+import thunk from 'redux-thunk'
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(thunk))
-)
 
-export default (store);
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  );
+
+
+const store = createStore(
+  persistedReducer,
+  enhancer
+);
+
+let persistor = persistStore(store);
+
+export { store, persistor };
