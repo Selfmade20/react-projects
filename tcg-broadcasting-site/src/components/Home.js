fc20d54ef7c8d624c02f0e9a44fb7bd1951f0e7b
Selfmade20 <tebogo1686@gmail.com>
2019-11-20 17:34:07 +0200

    Styling

--- tcg-broadcasting-site/src/components/Home.js
+++ tcg-broadcasting-site/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import phone from '../assets/phone.jpg'
+import '../App.css'
 
 const Styles = styled.div`
 .wrapper {
@@ -8,17 +9,42 @@ const Styles = styled.div`
     font-family: sans serif;
     text-align: left;
     color: white;
-    font-weight: bold;
-    color:blue;
-    
 }
+
+.submit-button {
+    font-size: 15px;
+    margin-top: 10px;
+    border-radius: 4px;
+}
+
+.form-control {
+    display: block;
+    width: 100%;
+    
+
 `
 
 
 export const Home = () => (
     <Styles>
         <div className="wrapper">
-            <h1 >The Coding Ground</h1>
+            <h1 className="header">WELCOME TO</h1>
+            <h1 className="header">THE CODING GROUND</h1>
+            <form id="survey-form">
+                <div className="form-group">
+                    <label className="name-label" for="name">Name</label>
+                    <input
+                        type="text"
+                        name="text_name"
+                        className="form-control"
+                        placeholder="Enter your name"
+                        required
+                    />
+                    <button type="submit" className="submit-button">Submit</button>
+                </div>
+            </form>
+
+
 
         </div>
 
