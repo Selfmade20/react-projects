98ddd7232a470dbb0ad40a0bcbb179ea512a08e5
Selfmade20 <tebogo1686@gmail.com>
2019-11-22 15:21:53 +0200

    Changes

--- thunk-example/src/Components/Toolbar/Toolbar.js
+++ thunk-example/src/Components/Toolbar/Toolbar.js
@@ -13,7 +13,7 @@ const toolbar = props => (
                 <ul>
                     <li><a href="https://ethw.org/Category:Computing_and_electronics?gclid=Cj0KCQjwjOrtBRCcARIsAEq4rW7lv372DilJCTmoEbFmVBKllyo3z-zNlhkaee30PL_XrAbJjcsHA5UaAuiaEALw_wcB">Computers</a></li>
                     <li><a href="https://sfbook.com/the-evolution-of-the-book.htm">Books</a></li>
-                    <li><a href="/">Users</a></li>
+                    <li><a href="/https://www.computerhope.com/jargon/u/user.htm">Users</a></li>
                 </ul>
             </div>
         </nav>
