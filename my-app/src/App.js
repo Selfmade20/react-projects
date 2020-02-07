6f23315c230ef6008f31da7d9e4c5fd59e9b6f29
montshejohn <mokgokongjohannes@gmail.com>
2020-02-07 08:50:03 +0200

    News app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <h2>Welcome To Codefest</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/login/login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export class Login extends React.Component {
+    constructor(props) {
+         super(props);
+        }
+        
+
+
+        render() {
+            return(
+                <div className="base-container">
+                    <div className="header">Login</div>
+                    <div className="content"></div>
+                </div>
+            )
+        }
+}
