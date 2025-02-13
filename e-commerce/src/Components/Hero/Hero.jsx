f01e4a5cf68688b8b09cf35a134f62f4578f8545
Selfmade20 <tebogo1686@gmail.com>
2025-02-13 19:42:18 +0200

    This are homepage section and only apply to the homepage and showcase some of the popular items

--- e-commerce/src/Components/Hero/Hero.jsx
+++ e-commerce/src/Components/Hero/Hero.jsx
@@ -3,13 +3,14 @@ import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
 import hero_image from '../Assets/hero_image.png'
+import playstation from '../Assets/playstation5.jpg'
 
 
 
 const Hero = () => {
   return (
     <div className='hero'> 
-    <div className="hero-left">
+    {/* <div className="hero-left">
         <h2>NEW ARRIVALS ONLY</h2>
         <div>
             <div className="hero-hand-icon">
@@ -23,9 +24,9 @@ const Hero = () => {
             <div>Latest Collection</div>
             <img src={arrow_icon} alt="" />
         </div>
-    </div>
+    </div> */}
     <div className="hero-right">
-    <img src={hero_image} alt="" />
+    <img src={playstation} alt="" />
     </div>
       
     </div>
